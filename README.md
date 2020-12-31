e37cc5d550a8de613cc468d3d082873cc651ffca
CFettig <claire.fettig@siu.edu>
2020-12-30 18:07:43 -0600

    added todo to readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 An online tool for language learners to practice pronunciation.
+
+TODO:
+- improve user security 
+- create practice pages for individual sounds
+- compare 2 words and get differing sounds (use cmudict/ARPAbet?)
+- create researcher view 
+- save data on users' preferences and practice
+- make photo prompts random
+- add written prompts
--- asr_tool/main.py
+++ asr_tool/main.py
@@ -12,9 +12,6 @@ def index():
 @main.route('/profile')
 @login_required
 def profile():
-    # posts = []  
-    # for post in Transcript.query.filter_by(user_id=current_user.id):
-    #     posts += post.serialize 
     posts = Transcript.query.filter_by(user_id=current_user.id)
     return render_template('profile.html', name=current_user.name, posts=posts)
 
@@ -23,15 +20,22 @@ def profile():
 def practice():
     if request.method=='POST':
         if 'actual_word' in request.form:
-            return 'comparing ' + request.form.get('actual_word') + ' and ' + request.form.get('user_word')
+            # return 'comparing ' + request.form.get('actual_word') + ' and ' + request.form.get('user_word')
+            return redirect(url_for('main.pronunciation'))
         else:
             save_transcript(request.form.get('transcript'))
-            return render_template('profile.html', name=current_user.name)
+            # return render_template('profile.html', name=current_user.name)
+            return redirect(url_for('main.profile'))
     else:
         return render_template('practice.html', user=current_user)
 
-@main.route('/save_transcript')
+@main.route('/pronunciation')
 @login_required
+def pronunciation():
+    pass
+
+# @main.route('/save_transcript')
+# @login_required
 def save_transcript(user_text):
     new_transcript = Transcript(text=user_text, user_id=current_user.id)
     db.session.add(new_transcript)
