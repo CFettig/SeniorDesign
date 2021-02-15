43a1aa2c01bd6e3bfd08c2702b66410305dd26c9
Markusovich <simon.markus9@gmail.com>
2021-02-15 01:58:55 -0600

    Added stuff

--- asr_tool/static/js/speech_to_text.js
+++ asr_tool/static/js/speech_to_text.js
@@ -10,7 +10,7 @@ function start_recording() {
   //toggles mic button to stop
   mic = document.getElementById('mic-toggle');
   mic.setAttribute('onclick', 'stop_recording()');
-  mic.innerHTML = "Pause";
+  mic.outerHTML = "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pause' viewBox='0 0 16 16'><path d='M6 3.5a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5zm4 0a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5z'/></svg>";
   asr.start();
 }
 
@@ -18,27 +18,27 @@ function stop_recording() {
   //toggles mic button to start
   mic = document.getElementById('mic-toggle');
   mic.setAttribute('onclick', 'start_recording()');
-  mic.innerHTML = "Record";
+  mic.innerHTML = "Record ";
 
   asr.stop();
 }
 
-asr.onresult = function(event) {
+asr.onresult = function (event) {
   interim_text = document.getElementById('interim-text');
   var new_text = document.getElementById('new-text');
   var interim = '';
   var transcript = '';
-  
-  
+
+
   for (var i = event.resultIndex; i < event.results.length; ++i) {
     if (event.results[i].isFinal) {
       //adds string to the span for final text
       transcript += event.results[i][0].transcript;
-    } 
+    }
     else {
       //adds string to the span for changing text
       interim += event.results[i][0].transcript;
-        }
+    }
   }
   new_text.textContent += transcript;
   interim_text.innerHTML = interim;
@@ -67,30 +67,30 @@ function rightClick(e) {
     document.getElementById('user-selection').value = select
 
     context.style.display = "block"
-    context.style.left = e.pageX + "px"; 
-    context.style.top = e.pageY + "px"; 
+    context.style.left = e.pageX + "px";
+    context.style.top = e.pageY + "px";
   }
 }
 
 //ajax call to save transcription text to database
 function send_transcript(transcript) {
   $.ajax({
-    data : {
+    data: {
       "transcript": transcript,
     },
-    type : 'POST',
-    url : '/save_transcript'
+    type: 'POST',
+    url: '/save_transcript'
   })
-  .done(function(data) {
-    if (data.error) {
+    .done(function (data) {
+      if (data.error) {
         $('#errorAlert').text(data.error).show();
         $('#successAlert').hide();
-    }
-    else {
+      }
+      else {
         $('#successAlert').text(data.name).show();
         $('#errorAlert').hide();
-    }
-});
+      }
+    });
 }
 
 function end_practice() {
