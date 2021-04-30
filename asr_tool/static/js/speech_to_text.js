9febbdf1d2d9f9648714c8543ad9cab185abd564
CFettig <claire.fettig@siu.edu>
2021-04-29 21:22:03 -0500

    displays error message for incompatible browsers

--- asr_tool/static/js/speech_to_text.js
+++ asr_tool/static/js/speech_to_text.js
@@ -1,21 +1,23 @@
-console.log('script loaded')
-	console.log(window.navigator)
-   window.navigator.mediaDevices.getUserMedia({ audio: true })
-            .then(stream => {
-            // Code for success
-            }).catch(err => {
-                if(err.includes("NotFoundError: Requested device not found"))
-                alert("Mic not detected")
-                else alert("Error recording audio")
-            })
+let asr;
 
 //js for asr listener
-var asr = new webkitSpeechRecognition();
+try {
+  asr = new webkitSpeechRecognition();
+}
+catch (err) {
+  document.getElementById('mydiv').setAttribute('style', 'display:none;')
+  document.getElementById('browser-incompatible').setAttribute('style', 'display:inline-block;')
+}
 
 //will continue to record even when speaker pauses
 asr.continuous = true;
 //will show interim words as speaker talks
 asr.interimResults = true;
+
+// //will continue to record even when speaker pauses
+// asr.continuous = true;
+// //will show interim words as speaker talks
+// asr.interimResults = true;
 
 function start_recording() {
 console.log('recording started')
@@ -24,7 +26,6 @@ console.log('recording started')
   mic.setAttribute('onclick', 'stop_recording()');
   mic.innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pause' viewBox='0 0 16 16'><path d='M6 3.5a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5zm4 0a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5z'/></svg>";
   asr.start();
-	console.log('asr started')
 }
 
 function stop_recording() {
@@ -35,11 +36,9 @@ function stop_recording() {
   mic.innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-mic-fill' viewBox='0 0 16 16'><path d='M5 3a3 3 0 0 1 6 0v5a3 3 0 0 1-6 0V3z' /><path fill-rule='evenodd' d='M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z' /></svg>";
 
   asr.stop();
-	console.log('asr stopped')
 }
 
 asr.onresult = function (event) {
-	console.log('has result')
   var new_text = document.getElementById('new-text');
   interim_text = document.getElementById('interim-text');
   var transcript = '';
@@ -72,7 +71,6 @@ asr.onresult = function (event) {
   }
 
   interim_text.innerHTML = interim;
-  // console.log(JSON.stringify(new_text.textContent))
   send_transcript(transcript)
 }
 
